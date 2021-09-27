997e3cdbc772077550ae9388192b73b463702990
Michal Tesnar <michal.tesnar007@gmail.com>
2021-09-27 17:06:16 +0200

    Update functionEvaluator.c

--- Assignments/Week4/1-FunctionEvaluator/functionEvaluator.c
+++ Assignments/Week4/1-FunctionEvaluator/functionEvaluator.c
@@ -1,17 +1,34 @@
 /*
- * file: <nameOfTheProgram>.c
+ * file: functionEvaluator.c
  * 
  * author: Michal Tešnar (m.tesnar@studen.rug.nl)>
  * 
- * Description: <add here>
+ * Description: This program defines three functions
+ * and executes them on a number in the order given
+ * by input.
  */
 
 
 #include <stdio.h>
 
+int functionF(int *x){
+	return 3*(*x)+1;
+}
+
+int functionG(int *x){
+	return *x/2;
+}
+
+int functionH(int *x){
+	return (*x)*(*x)-(*x);
+}
+
 int main(int argc, char **argv)
 {
-	printf("Hello world!\n");
+	int n; //input variable
+	char functionOrder[]; //commands to execute on the variable
+	scanf("%i %s", &n, functionOrder);
+	printf("%i %i %i", functionF(&n), functionG(&n), functionH(&n));
 	return 0;
 }
 
