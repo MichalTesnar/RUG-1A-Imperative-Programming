6e3842409781372d4cf2ff1fd3e82c3ba33e5396
Michal Tesnar <michal.tesnar007@gmail.com>
2021-09-29 12:16:59 +0200

    Week 4 Done
    Final versions submitted

--- Assignments/Week4/3-PalindromicDivisors/palindromicDivisors.c
+++ Assignments/Week4/3-PalindromicDivisors/palindromicDivisors.c
@@ -9,9 +9,11 @@
 
 #include <stdio.h>
 
+//checks if number is a palindrom
 int isPalindrom(int j){
 	int manipulated = j;
 	int reverse = 0;
+
 	while(manipulated != 0){
 		reverse = reverse*10 + manipulated%10;
 		manipulated /= 10;
@@ -29,23 +31,32 @@ int main(int argc, char **argv)
 	int a, b; //input range
 	scanf("%i %i", &a, &b);
 	
-	int amount = 0;
+	int amount = 0; //counts the amount of numbers which satisfy the condition
 
+	//search range, check number of palindromic divisors
 	for(int i = a; i <= b; i++){
 		int counter = 0;
-		for(int j=1; j*j <= i; j++){
+		
+		for(int j = 1; j*j <= i; j++){
+			
 			if(i%j == 0 && isPalindrom(j) == 1){
 				counter++;
 			}
+			
 			if(i%j == 0 && isPalindrom(i/j) == 1 && i/j != 1){
 				counter++;
 			}
 			
+			//if the number of palindromic divisors is greater than 1, we do not have to count anymore
+			if(counter > 1){
+				break;
+			}	
 		}
+		
+		//if the number of palindromic divisors is 1, we increase the counter
 		if(counter == 1){
 			amount++;
 		}
-		//printf("%i %i\n", i, counter);
 	}	
 	
 	printf("%i\n", amount);
